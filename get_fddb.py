d5d6ec371acc6ba9095314c4d1893e374de2ab6d
Devon Ulrich <dpulrich@gmail.com>
2021-11-28 20:28:34 -0500

    Updated FDDB download script

--- get_fddb.py
+++ get_fddb.py
@@ -4,7 +4,7 @@ import urllib.request as req
 # downloads dataset from http://vis-www.cs.umass.edu/fddb/
 
 def download(force=False, path='.'):
-    print('Downloading FDDB into new folder ./fddb/')
+    print('Downloading FDDB into new folder ' + path + '/fddb/')
     if os.path.exists(path + '/fddb') and not force:
         print('FDDB already downloaded.')
         return
@@ -15,12 +15,16 @@ def download(force=False, path='.'):
     print('Extracting tar file...')
 
     os.system('mkdir ' + path + '/fddb')
-    os.system('tar xzf tmp.tar.gz -C ' + path + '/fddb')
+    os.system('tar xzf ' + path + '/tmp.tar.gz -C ' + path + '/fddb')
+
+    os.system('rm ' + path + '/tmp.tar.gz')
+
+    print('Done!')
 
 if __name__ == "__main__":
     path = '.'
     if os.path.isfile('./dataset_path'):
         with open('./dataset_path') as f:
-            path = f.readline()
+            path = f.readline()[:-1]
         
     download(force=True, path=path)
