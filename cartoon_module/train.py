da09d48db146ef8c35ecc5ebb44208e035242c18
Devon Ulrich <dpulrich@gmail.com>
2021-12-08 02:20:18 -0500

    Improved training
    Might need to change final activation to softmax and then
    change from_logits (that's what I had to get 90% accuracy)

--- cartoon_module/train.py
+++ cartoon_module/train.py
@@ -7,19 +7,27 @@ from datagen import CartoonDataGenerator
 ANIME_PATH = '/scratch/network/dulrich/personai_icartoonface_dettrain'
 
 def train():
-    base_model = keras.applications.VGG16(include_top=False, weights='imagenet')
-    base_model.trainable = True
+    base_model = keras.applications.VGG16(include_top=False, weights='imagenet', 
+        input_shape=(224, 224, 3))
+    base_model.summary()
+    base_model.trainable = False
     model_input = keras.Input(shape=(224, 224, 3))
+    x = tf.cast(model_input, tf.float32)
+    x = keras.applications.vgg16.preprocess_input(x)
+    
     x = base_model(model_input, training=False)
-    x = keras.layers.Convolution2D(1, 2, 2, activation='relu', name='newconv_1')(x)
+    # x = keras.layers.Convolution2D(1, 2, 2, activation='relu', name='newconv_1')(x)
     x = keras.layers.Flatten()(x)
-    x = keras.layers.Dense(1, activation='sigmoid')(x)
+    x = keras.layers.Dense(1)(x)
 
     model = keras.Model(model_input, x)
-    model.compile(optimizer=keras.optimizers.Adam(), loss='binary_crossentropy', metrics=['accuracy'])# , metrics=[keras.metrics.Accuracy(), keras.metrics.TruePositives()])
+    crossentropy = keras.losses.BinaryCrossentropy(from_logits=True)
+    model.compile(optimizer=keras.optimizers.Adam(), loss=crossentropy, metrics=['accuracy'])
+    model.summary()
 
     gen = CartoonDataGenerator(ANIME_PATH)
     model.fit(gen, verbose=1, use_multiprocessing=True)
+    model.save('./modelout')
 
 if __name__ == '__main__':
     train()
